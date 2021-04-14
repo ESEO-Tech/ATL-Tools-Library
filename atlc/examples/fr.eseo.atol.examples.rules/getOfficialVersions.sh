d61176226ac1f80e6231fcf9a26846e2247fc912
Théo Le Calvar <tlc@kher.nl>
2021-04-14 11:07:12 +0200

    create page with example from atlc/web

--- atlc/examples/fr.eseo.atol.examples.rules/getOfficialVersions.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-unzip -p ~/.p2/pool/plugins/org.eclipse.uml2.uml.resources_5.2.0.v20170227-0935.jar metamodels/Ecore.metamodel.uml > testcases/ecore/ecore-out-official.uml
-unzip -p ~/.p2/pool/plugins/org.eclipse.uml2.uml.resources_5.2.0.v20170227-0935.jar metamodels/UML.metamodel.uml > testcases/uml/uml-out-official.uml
-
-
-
